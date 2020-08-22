97cb3928070853ed2a26fbb29473affac47da910
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-08-22 08:07:43 -0700

    added google oauth20

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+
+  
 const express = require('express');
-const app=express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-});
+const app = express();
 
-const PORT=process.env.PORT || 5000
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))   ;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
